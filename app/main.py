ee0d80a9e07258150b92b89ec1344e903523a5c2
jshlomov <jonatanshv@gmail.com>
2024-11-14 10:04:40 +0200

    correct flask

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from flask import Flask
 from app.routs.get_data_rout import data_blueprint
 
 app = Flask(__name__)
-app.register_blueprint(data_blueprint, url_prefix='/app')
+app.register_blueprint(data_blueprint, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run()
--- app/routs/get_data_rout.py
+++ app/routs/get_data_rout.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, request, jsonify
 
+from app.repository.mongo_repository import insert_message
 
 data_blueprint = Blueprint('data', __name__)
 
 
 @data_blueprint.route('/email', methods=['POST'])
 def new_member():
-    mem = request.json
+    message = request.json
+    print(message)
+    insert_message(message)
     return jsonify("info for new member recived"), 200
