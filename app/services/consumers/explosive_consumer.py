98c1fc87b3c8a82e1b9f3fe03726ade8c7fb193b
jshlomov <jonatanshv@gmail.com>
2024-11-14 16:30:24 +0200

    final consumer

--- app/services/consumers/explosive_consumer.py
+++ app/services/consumers/explosive_consumer.py
@@ -2,13 +2,19 @@ import os
 
 from dotenv import load_dotenv
 
+from app.repository.postgres_repository.suspicious_hostage_repository import insert_hostage_content
 from app.services.kafka_tools.consumer import consume
+from app.services.make_email_service import insert_all_message, convert_json_to_explosive_contents
 
 load_dotenv(verbose=True)
 
 def consume_explosive():
     consume(
-        topic_name=os.environ['TOPIC_MESSAGES_EXPLOSIVE_NAME']
+        topic_name=os.environ['TOPIC_MESSAGES_EXPLOSIVE_NAME'],
+        func=insert_all_message(
+            convert_sentences_func=convert_json_to_explosive_contents,
+            insert_sentences_func=insert_hostage_content
+        )
     )
 
 if __name__ == '__main__':
--- app/services/consumers/hostage_consumer.py
+++ app/services/consumers/hostage_consumer.py
@@ -1,13 +1,20 @@
 import os
 
 from dotenv import load_dotenv
+
+from app.repository.postgres_repository.suspicious_hostage_repository import insert_hostage_content
 from app.services.kafka_tools.consumer import consume
+from app.services.make_email_service import insert_all_message, convert_json_to_hostage_contents
 
 load_dotenv(verbose=True)
 
 def consume_hostage():
     consume(
-        topic_name=os.environ['TOPIC_MESSAGES_HOSTAGE_NAME']
+        topic_name=os.environ['TOPIC_MESSAGES_HOSTAGE_NAME'],
+        func=insert_all_message(
+            convert_sentences_func=convert_json_to_hostage_contents,
+            insert_sentences_func=insert_hostage_content
+        )
     )
 
 if __name__ == '__main__':
