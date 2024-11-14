5f02a78632856ba8556f61d34b5b2973ac854843
jshlomov <jonatanshv@gmail.com>
2024-11-14 14:52:37 +0200

    init file updated for postgres

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
-from .Person import Person
+from .Person import DeviceInfo
 from .Location import Location
 from .DeviceInfo import DeviceInfo
 from .SuspiciousHostageContent import SuspiciousHostageContent
