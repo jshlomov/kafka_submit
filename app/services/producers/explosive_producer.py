aee1342155cadcdb64f743938df529e98b4f72e1
jshlomov <jonatanshv@gmail.com>
2024-11-14 12:13:32 +0200

    create producers for all messages, explosive and hostage

---
+++ app/services/producers/explosive_producer.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+from app.services.kafka_tools.producer import produce
+
+load_dotenv(verbose=True)
+
+def produce_explosive_message(message):
+    produce(
+        topic_name=os.environ(['TOPIC_MESSAGES_EXPLOSIVE_NAME']),
+        message=message,
+        key=message['email']
+    )
---
+++ app/services/producers/hostage_producer.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+from app.services.kafka_tools.producer import produce
+
+load_dotenv(verbose=True)
+
+def produce_hostage_message(message):
+    produce(
+        topic_name=os.environ(['TOPIC_MESSAGES_HOSTAGE_NAME']),
+        message=message,
+        key=message['email']
+    )
